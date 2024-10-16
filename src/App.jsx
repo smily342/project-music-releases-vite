62ad96be7d7d7d7d0ac405c99ea2b90fc63e31b7
smily342 <B_dadi@ik.me>
2024-10-16 21:06:01 +0200

    correction

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import data from "./data.json";
 import "./index.css";
 import Album from "./album";
 
-
-
 export const App = () => {
-
   const albums = data.albums.items;
 
   return (
@@ -19,29 +16,9 @@ export const App = () => {
         {albums.map((album) => (
           <Album key={album.id} album={album} />
         ))}
-
       </div>
     </div>
   );
 };
 
-<<<<<<< HEAD
-=======
-// Album component to display album details
-const Album = ({ album }) => {
-  return (
-    <div className="album-item">
-      <h2>{album.name}</h2>  {/* Display the album name */}
-      <img src={album.images[0].url} alt={album.name} width="200px" />  {/* Album cover */}
-
-      <p>Artists: {album.artists.map(artist => artist.name).join(", ")}</p>  {/* Artists */}
-      <a href={album.external_urls.spotify} target="_blank" rel="noopener noreferrer">
-        Listen on Spotify
-      </a>  {/* Spotify link */}
-    </div>
-  );
-};
-
->>>>>>> 9d4bff3 (added components and styling)
 export default App;
-
--- src/album.jsx
+++ src/album.jsx
@@ -1,16 +1,10 @@
-<<<<<<< HEAD
 /* eslint-disable react/prop-types */
+import React from "react";
 import ArtistName from "./ArtistName";
 import "./index.css";
 import dotsIcon from "./assets/icons/dots.svg";
 import heartIcon from "./assets/icons/heart.svg";
 import playIcon from "./assets/icons/play.svg";
-=======
-
-import React from "react";
-import ArtistName from "./ArtistName";
-
->>>>>>> 9d4bff3 (added components and styling)
 
 const Album = ({ album }) => {
     const { name, images, artists, external_urls } = album;
@@ -26,7 +20,6 @@ const Album = ({ album }) => {
             <div className="overlay"></div>
             <div className="icons">
                 <img className="icon-small" src={heartIcon} alt="heart-icon" />
-                {/* Wrap the play icon in an anchor tag to make it clickable */}
                 {external_urls && external_urls.spotify && (
                     <a href={external_urls.spotify} target="_blank" rel="noopener noreferrer">
                         <img className="icon-big" src={playIcon} alt="play-icon" />
@@ -35,19 +28,17 @@ const Album = ({ album }) => {
                 <img className="icon-small" src={dotsIcon} alt="dots-icon" />
             </div>
 
-            {/* Make the album title clickable, linking to Spotify */}
             <h2>
                 {external_urls && external_urls.spotify && (
                     <>
-                        {console.log("Album URL: ", album.external_urls.spotify)}
-                        <a className="album-link" href={album.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+                        {console.log("Album URL: ", external_urls.spotify)}
+                        <a className="album-link" href={external_urls.spotify} target="_blank" rel="noopener noreferrer">
                             {name}
                         </a>
                     </>
                 )}
             </h2>
 
-            {/* Render the artist names */}
             <h3> <ArtistName artists={artists} /></h3>
 
         </div>
@@ -55,6 +46,3 @@ const Album = ({ album }) => {
 };
 
 export default Album;
-
-
-
